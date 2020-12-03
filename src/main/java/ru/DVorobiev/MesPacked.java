524c1cc346183c408ce0cb34b5d857a031105d9a
DVorobiev1968 <vdv@rdturbo.ru>
2020-12-03 10:42:19 +0500

    Изменил обозначение имен методов класса MesPacked

--- src/main/java/ru/DVorobiev/MesPacked.java
+++ src/main/java/ru/DVorobiev/MesPacked.java
@@ -2,7 +2,7 @@ package ru.DVorobiev;
 
 import java.util.Formatter;
 
-public class mesPacked {
+public class MesPacked {
 	private int i_idNode;		// идентификатор узла
 	private int i_codeCommand;	// код команды присваивается в зависимости от протокола работы узла
 	public int i_code_answer;	// код ответа от узла
@@ -31,11 +31,11 @@ public class mesPacked {
 	// включаем непосредственно само сообщение
 	public Message message;
 
-	public String getS_message() {
+	public String getSMessage() {
 		return s_message;
 	}
 
-	public void setS_message() {
+	public void setSMessage() {
 		f.format("%d;%d;%d;%d;%d;%d;%f;",i_idNode,i_codeCommand,i_code_answer,h_idObj,h_idSubObj,i_typeData,d_value);
 		String s_temp=f.toString();
 		int i_len=s_temp.length();
@@ -43,68 +43,68 @@ public class mesPacked {
 		this.s_message = s_temp+s_len;
 	}
 
-	public int getI_codeCommand() {
+	public int getICodeCommand() {
 		return i_codeCommand;
 	}
 
-	public void setI_codeCommand(int i_codeCommand) {
+	public void setICodeCommand(int i_codeCommand) {
 		this.i_codeCommand = i_codeCommand;
-		this.setS_command(cl.errMessage(i_codeCommand));
+		this.setSCommand(cl.errMessage(i_codeCommand));
 	}
 
-	public int getI_code_answer() {
+	public int getICodeAnswer() {
 		return i_code_answer;
 	}
 
-	public void setI_code_answer(int i_code_answer) {
+	public void setICodeAnswer(int i_code_answer) {
 		this.i_code_answer = i_code_answer;
 	}
 
-	public double getD_value() {
+	public double getDValue() {
 		return d_value;
 	}
 
-	public void setD_value(double d_value) {
+	public void setDValue(double d_value) {
 		this.d_value = d_value;
 	}
 
-	public int getI_idNode() {
+	public int getIIdNode() {
 		return i_idNode;
 	}
 
-	public void setI_idNode(int i_idNode) {
+	public void setIIdNode(int i_idNode) {
 		this.i_idNode = i_idNode;
 	}
 
-	public String getS_command() {
+	public String getSCommand() {
 		return s_command;
 	}
 
-	public void setS_command(String s_command) {
+	public void setSCommand(String s_command) {
 		this.s_command = s_command;
 	}
 
-	public int getH_idObj() {
+	public int getHIdObj() {
 		return h_idObj;
 	}
 
-	public void setH_idObj(int h_idObj) {
+	public void setHIdObj(int h_idObj) {
 		this.h_idObj = h_idObj;
 	}
 
-	public int getH_idSubObj() {
+	public int getHIdSubObj() {
 		return h_idSubObj;
 	}
 
-	public void setH_idSubObj(int h_idSubObj) {
+	public void setHIdSubObj(int h_idSubObj) {
 		this.h_idSubObj = h_idSubObj;
 	}
 
-	public int getI_typeData() {
+	public int getITypeData() {
 		return i_typeData;
 	}
 
-	public void setI_typeData(int i_typeData) {
+	public void setITypeData(int i_typeData) {
 		this.i_typeData = i_typeData;
 	}
 
@@ -112,7 +112,7 @@ public class mesPacked {
 		f.format("Node:%d;Code:%d;Value:%3.6f\n",i_idNode,i_code_answer,d_value);
 		return f.toString();
 	}
-	mesPacked() {
+	MesPacked() {
 		i_idNode=1;
 		i_codeCommand=CODE_START;
 		s_command="";
@@ -121,6 +121,6 @@ public class mesPacked {
 		i_typeData=0;
 		d_value=1;
 		f=new Formatter();
-		setS_message();
+		setSMessage();
 	}
 }
--- src/main/java/ru/DVorobiev/Server.java
+++ src/main/java/ru/DVorobiev/Server.java
@@ -4,12 +4,10 @@ import java.io.*;
 import java.net.*;
 import java.util.Formatter;
 
-import ru.DVorobiev.Message;
-
 public class Server {
   public void run() {
 	try {
-	   	mesPacked msgRequest = new mesPacked();
+	   	MesPacked msgRequest = new MesPacked();
 	   	int serverPort = msgRequest.port;
 	   	Formatter f =new Formatter();  				//объявление объекта для форматирования
 		double d_value =0;
@@ -26,17 +24,17 @@ public class Server {
 			PrintWriter toClient = new PrintWriter(server.getOutputStream(),true);
 			BufferedReader fromClient =	new BufferedReader(new InputStreamReader(server.getInputStream()));
 			String s_temp= fromClient.readLine();
-			msgRequest.setS_message();
+			msgRequest.setSMessage();
 			System.out.println("Server received: " + s_temp);
-			d_value = msgRequest.getD_value();
-			msgRequest.setD_value(d_value++);
-			msgRequest.setI_code_answer(0);
+			d_value = msgRequest.getDValue();
+			msgRequest.setDValue(d_value++);
+			msgRequest.setICodeAnswer(0);
 			f.format("Node:%d;Code:%d;Value:%3.6f\n",
-						msgRequest.getI_idNode(),
-						msgRequest.getI_code_answer(),
-						msgRequest.getD_value());
-			msgRequest.setS_message();
-			toClient.println(msgRequest.getS_message());
+						msgRequest.getIIdNode(),
+						msgRequest.getICodeAnswer(),
+						msgRequest.getDValue());
+			msgRequest.setSMessage();
+			toClient.println(msgRequest.getSMessage());
 		}
 	}
 	catch(UnknownHostException ex) {
