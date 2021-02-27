42bf2e3e785ba305065d7c82c3ef12e48dc6b932
OdinKostya <oka54@mail.ru>
2021-02-27 23:02:19 +0300

    Рефакторинг: - именование по Java convention - порядок элементов класса по Java convention - инкапсуляция - lombok для сокращения кода - стандартный logger - объявление переменных ближе к месту использования

---
+++ src/main/java/ru/DVorobiev/MessagePacked.java
@@ -0,0 +1,70 @@
+package ru.dvorobiev;
+
+import java.util.Formatter;
+import lombok.Data;
+
+@Data
+public class MessagePacked {
+    // коды ошибок и их описание
+    public static final int OK = 1;
+    public static final int ERR = 0;
+    // коды команд
+    public static final int CODE_START = 0x1;
+    public static final int CODE_STOP = 0x2;
+    /** Объект для форматирования */
+    private static final Formatter FORMATTER = new Formatter();
+
+    /** Идентификатор узла */
+    private int nodeId;
+    /** Код команды присваивается в зависимости от протокола работы узла */
+    private int commandCode;
+    /** Код ответа от узла */
+    public int answerCode;
+    /** Команда */
+    private String command;
+
+    /** Идентификатор объекта */
+    private int objectId;
+    /** Идентификатор субобъекта */
+    private int subObjectId;
+    /** Тип данных объекта */
+    private int objectDataType;
+    /** Возвращаемое значение */
+    public double value;
+    /** Контрольная сумма */
+    private int check;
+
+    MessagePacked() {
+        nodeId = 1;
+        commandCode = CODE_START;
+        command = "";
+        objectId = 0x0;
+        subObjectId = 0x0;
+        objectDataType = 0;
+        value = 1;
+    }
+
+    /**
+     * Строка получаемая из буфера
+     * @return Строка получаемая из буфера
+     */
+    public String messagePacked() {
+        FORMATTER.format(
+                "%d;%d;%d;%d;%d;%d;%f;",
+                nodeId, commandCode, answerCode, objectId, subObjectId, objectDataType, value);
+        String temp = FORMATTER.toString();
+        // учитываем служебные символы может меняться из-за кодировки utf-8(+3)/ascii(+4)
+        String realLength = Integer.toString(temp.length() + 3);
+        return temp + realLength;
+    }
+
+    public void setCommandCode(int commandCode) {
+        this.commandCode = commandCode;
+        this.setCommand(Classif.errMessage(commandCode));
+    }
+
+    public String getInfo() {
+        FORMATTER.format("Node:%d;Code:%d;Value:%3.6f\n", nodeId, answerCode, value);
+        return FORMATTER.toString();
+    }
+}
---
+++ src/main/java/ru/DVorobiev/report/ReportExcel.java
@@ -0,0 +1,133 @@
+package ru.dvorobiev.report;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import ru.dvorobiev.model.DataSignal;
+import ru.dvorobiev.model.DataSignalDAO;
+
+/** класс для вывода информации в Excel */
+public class ReportExcel {
+    public HSSFWorkbook workbook; // объект рабочей книги
+
+    public HSSFSheet sheet;
+    public List<DataSignal> list;
+    /** Сообщение об ошибке */
+    public String errMessage;
+    /** Путь с именем файла данных по умолчанию ./(name_sheet).xls */
+    public String path;
+    public static final int OK = 0;
+    public static final int ERR = -1;
+    public static final int EMPTY = -2;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    /** @param name_sheet : имя листа рабочей книги */
+    public ReportExcel(String name_sheet) {
+        workbook = new HSSFWorkbook();
+        sheet = workbook.createSheet(name_sheet);
+        this.list = DataSignalDAO.dataSignalList();
+        path = new String();
+        path = String.format("./%s.xls", name_sheet);
+    }
+
+    /**
+     * Метод формирует отчет в Excel
+     *
+     * @return : OK, ERR - в случае исключения, EMPTY - нет данных
+     */
+    public int CreateReport() {
+        int rownum = 0;
+        Cell cell;
+        Row row;
+
+        HSSFCellStyle style = createStyleForTitle(workbook);
+        row = sheet.createRow(rownum);
+        // Date
+        cell = row.createCell(0, CellType.STRING);
+        cell.setCellValue("DataTime");
+        cell.setCellStyle(style);
+        // miliseconds
+        cell = row.createCell(1, CellType.STRING);
+        cell.setCellValue("ms");
+        cell.setCellStyle(style);
+        // Id_Node
+        cell = row.createCell(2, CellType.STRING);
+        cell.setCellValue("ID Node");
+        cell.setCellStyle(style);
+        // Id_Obj
+        cell = row.createCell(3, CellType.STRING);
+        cell.setCellValue("ID Object");
+        cell.setCellStyle(style);
+        // Value
+        cell = row.createCell(4, CellType.STRING);
+        cell.setCellValue("valueSource");
+        cell.setCellStyle(style);
+        // Value
+        cell = row.createCell(5, CellType.STRING);
+        cell.setCellValue("value");
+        cell.setCellStyle(style);
+
+        // Data
+        for (DataSignal item : list) {
+            rownum++;
+            row = sheet.createRow(rownum);
+            cell = row.createCell(0, CellType.STRING);
+            cell.setCellValue(item.getDateString(item.getDate()));
+            cell = row.createCell(1, CellType.NUMERIC);
+            cell.setCellValue(item.getTime());
+            cell = row.createCell(2, CellType.NUMERIC);
+            cell.setCellValue(item.getNodeId());
+            cell = row.createCell(3, CellType.NUMERIC);
+            cell.setCellValue(item.getObjectId());
+            cell = row.createCell(4, CellType.NUMERIC);
+            cell.setCellValue(item.getValueSource());
+            cell = row.createCell(5, CellType.NUMERIC);
+            cell.setCellValue(item.getValue());
+        }
+        if (rownum > 0) {
+            try {
+                File file = new File(String.format("%s", path));
+                file.getParentFile().mkdirs();
+
+                FileOutputStream outFile = new FileOutputStream(file);
+                workbook.write(outFile);
+                errMessage = String.format("Created file: %s", file.getAbsolutePath());
+                return OK;
+            } catch (IOException e) {
+                errMessage = e.getMessage();
+                return ERR;
+            }
+        }
+        errMessage = String.format("Count rows: %d. Report not create.", rownum);
+        return EMPTY;
+    }
+
+    /**
+     * Задаем необходимый стилб для работы в документе
+     *
+     * @param workbook: путь и имя файла с выгрухкой данных
+     * @return style: вохвращем объект стиля документа
+     */
+    private HSSFCellStyle createStyleForTitle(HSSFWorkbook workbook) {
+        HSSFFont font = workbook.createFont();
+        font.setBold(true);
+        HSSFCellStyle style = workbook.createCellStyle();
+        style.setFont(font);
+        return style;
+    }
+}
